497065d6a83d77bf06c2d7b23e897f3afba635a0
GensokyoPerson <108526422+GensokyoPerson@users.noreply.github.com>
2022-07-11 21:09:15 +0300

    1.20_Views_Images_Static_Files_Razor

---
+++ Kiaki/Kia/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Kia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           string[] str =  Directory.GetFiles("wwwroot/Images"); //Начальная директория это папка проекта
+            for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = str[i].Remove(0, str[i].LastIndexOf('\\') + 1); //Возращает полный wwwroot/Images/name.jpg а нужено только name.jpg
+            }
+            return View(str);
+        }
+    }
+}
