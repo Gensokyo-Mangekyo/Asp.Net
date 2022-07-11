cdef50e06e22a50e036eba0350de19cbe4460f18
GensokyoPerson <samplay4ik@gmail.com>
2022-07-11 20:48:48 +0300

    1.10 Controllers Attributes

---
+++ Asp.Net/Asp.Net/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.Net.Controllers
+{
+    //[NonController] говорит что класс не является конроллером
+    public class HomeController : Controller
+    {
+        [ActionName("Sakuya")] //Теперь перейди по этому адресу /Home/Sakuya и он тебе не найдёт такое представление
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [NonAction] //Метод не является действием контроллера
+        public IActionResult Action()
+        {
+            return View();
+        }
+
+        //Типы http запросов  [HttpGet], [HttpPost], [HttpPut], [HttpPatch], [HttpDelete] и [HttpHead]
+    }
+}
